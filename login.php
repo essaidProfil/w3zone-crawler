603ec5f539b9bc6454e899fd5197808e47696ccc
ebo <essaid.profil@gmail.com>
2018-07-09 00:34:33 +0200

    Modifiying the login.php file

--- login.php
+++ login.php
@@ -1,6 +1,31 @@
 <?php
+require_once 'vendor/autoload.php';
 
+use w3zone\Crawler\{Crawler, Services\phpCurl};
 
+$crawler = new Crawler(new phpCurl);
+
+$url = 'http://domaine.com/login';
+$response = $crawler->get($url)->dumpHeaders()->run();
+
+preg_match('#<input name="token".*?value="(.*?)"#', $response['body'], token);
+
+$url = 'https://domaine.com/session';
+$post['submit'] = 'Sign in';
+$post['token'] = token[1];
+$post['login'] = '';
+$post['password'] = '';
+
+$response = $crawler
+    ->post(['url' => $url, 'data' => $post])
+    ->cookies($response['cookies'], 'w+r')
+    ->initialize([
+        CURLOPT_FOLLOWLOCATION => true
+    ])
+    ->dumpHeaders()
+->run();
+
+var_dump($response);
 
 
 ?>
